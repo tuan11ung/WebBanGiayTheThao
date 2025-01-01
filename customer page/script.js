c9a60093460751d4abdc2ce0047cc1728d3fddde
Hunghihi <nguyenduytuan11ung@gmail.com>
2025-01-01 17:54:38 +0700

    done customer page

--- customer page/script.js
+++ customer page/script.js
@@ -13,21 +13,21 @@ var MenuItems = document.getElementById("MenuItems");
         }
 
  // <!-- js for product gallery -->
-        var productImg = document.getElementById("productImg");
-        var smallImg = document.getElementsByClassName("small-img");
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("small-img");
     
-            smallImg[0].onclick = function() {
-                productImg.src = smallImg[0].src;
-            }
-            smallImg[1].onclick = function() {
-                productImg.src = smallImg[1].src;
-            }
-            smallImg[2].onclick = function() {
-                productImg.src = smallImg[2].src;
-            }
-            smallImg[3].onclick = function() {
-                productImg.src = smallImg[3].src;
-            }
+        smallImg[0].onclick = function() {
+            productImg.src = smallImg[0].src;
+        }
+        smallImg[1].onclick = function() {
+            productImg.src = smallImg[1].src;
+        }
+        smallImg[2].onclick = function() {
+            productImg.src = smallImg[2].src;
+        }
+        smallImg[3].onclick = function() {
+            productImg.src = smallImg[3].src;
+        }
 
 // <!-- js for toggle form -->
 var LoginForm = document.getElementById("LoginForm");
